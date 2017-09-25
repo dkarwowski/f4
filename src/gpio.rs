9fa899ebe01205853fe6a7c538649278bd19687f
David Karwowski <karwowski.david@gmail.com>
2017-09-24 23:08:04 -0400

    adding documentation

--- src/gpio.rs
+++ src/gpio.rs
@@ -5,8 +5,65 @@
 //! # TODO
 //!
 //! Need to clean up the Mode, Speed, Type, Output, and PuPd resistors.
-//! Currently uses bit flags, but doesn't actually ensure that invalid bit
-//! flags aren't actually used.
+//! Only uses bit flags for the pins, everything else is handled with an
+//! `enum`. Allows enabling/disabling multiple pins at once.
+//!
+//! # Example
+//!
+//! ``` ignore,no_run
+//! #![feature(used)]
+//! #![no_std]
+//!
+//! extern crate cortex_m;
+//! extern crate cortex_m_rt;
+//! extern crate f4;
+//!
+//! use cortex_m::asm;
+//! use f4::stm32f429x::{GPIOG, RCC};
+//! use f4::gpio::{self, GPIO};
+//!
+//! fn main() {
+//!     cortex_m::interrupt::free(
+//!         |cs| {
+//!             let gpiog = GPIOG.borrow(cs);
+//!             let rcc = RCC.borrow(cs);
+//!
+//!             rcc.ahb1enr.modify(|_, w| w.gpiogen().enabled());
+//!             gpiog.pin_enable(
+//!                 gpio::PIN13,
+//!                 gpio::Mode::Out,
+//!                 gpio::Speed::Low,
+//!                 gpio::Output::PushPull,
+//!                 gpio::PuPd::NoPull
+//!             );
+//!
+//!             let count = 13 * 5000;
+//!
+//!             loop {
+//!                 if gpiog.odr.read().odr13().bits() != 0u8 {
+//!                     gpiog.pin_reset(gpio::PIN13);
+//!                 } else {
+//!                     gpiog.pin_set(gpio::PIN13);
+//!                 }
+//!                 delay(count);
+//!             }
+//!         },
+//!     );
+//! }
+//!
+//! fn delay(count: u32) {
+//!     for _ in 0..count { cortex_m::asm::nop() }
+//! }
+//!
+//! #[allow(dead_code)]
+//! #[link_section = ".rodata.interrupts"]
+//! #[used]
+//! static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
+//!
+//! extern "C" fn default_handler() {
+//!     asm::bkpt();
+//! }
+//! ```
 
 use stm32f429x::{Gpioa, Gpiob, Gpioc, Gpiod, Gpioe, Gpiof, Gpiog, Gpioh, Gpioi, Gpioj, Gpiok};
 use cast::u32;
